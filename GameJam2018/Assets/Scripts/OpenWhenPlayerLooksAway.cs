b8b9d14e54f7b2b2c1ee30b66bd2e8c8bc41721c
AlexanderWardlow <37199749+AlexanderWardlow@users.noreply.github.com>
2018-03-10 14:21:05 -0500

    doorIfSeen

--- GameJam2018/Assets/Scripts/OpenWhenPlayerLooksAway.cs
+++ GameJam2018/Assets/Scripts/OpenWhenPlayerLooksAway.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class OpenWhenPlayerLooksAway : MonoBehaviour {
 
+	public Animator A;
+	public string ifSeen;
+	public string ifNotSeen;
+	public bool doToggle;
+	private Renderer R;
+
 	// Use this for initialization
 	void Start () {
-		
+		R = transform.gameObject.GetComponent<Renderer> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (R.isVisible) {
+			A.SetBool(ifSeen,true);
+			if (doToggle) {
+				A.SetBool(ifNotSeen,false);
+			}
+		}
+		else {
+			A.SetBool(ifNotSeen,true);
+			if (doToggle) {
+				A.SetBool(ifSeen,false);
+			}
+		}
 	}
 }
