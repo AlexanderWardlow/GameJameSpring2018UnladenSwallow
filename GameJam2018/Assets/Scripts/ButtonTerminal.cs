e98fdcb4f6ea44765b70c2bfeae9a40f1e5f77e0
AlexanderWardlow <37199749+AlexanderWardlow@users.noreply.github.com>
2018-03-10 11:07:50 -0500

    ObjectPickUpC#conversion

--- GameJam2018/Assets/Scripts/ButtonTerminal.cs
+++ GameJam2018/Assets/Scripts/ButtonTerminal.cs
@@ -2,20 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-
 public class ButtonTerminal : MonoBehaviour {
 	public Animator anim;
 	public string correctCubeColor;
+	public bool cantPickupAfter;
+
+	public Animator[] anim2;
+	public string[] animOn;
+	public string[] animOff;
+	public Transform[] activeOn;
+	public Transform[] activeOff;
+
 	void OnTriggerEnter (Collider other) {
 		//FIXME need to convert the objectControl to a c# class and check if object can be picked up
 		if (!anim.GetBool("boxPresent")) {
 			if (other.gameObject.tag == "PickUpAble") {
+				if (cantPickupAfter) {
+					other.gameObject.tag = "Untagged";
+				}
+
 				anim.SetBool ("boxPresent", true);
 				if (other.gameObject.name.IndexOf(correctCubeColor) != -1) {
 					anim.SetBool ("CorrectAnswer", true);
 					anim.SetBool ("WrongAnswer", false);
+					CorrectEffect ();
+
 				}
 				else {
 					anim.SetBool ("CorrectAnswer", false);
@@ -31,5 +42,20 @@ public class ButtonTerminal : MonoBehaviour {
 				anim.SetBool ("WrongAnswer", false);
 				anim.SetBool ("boxPresent", false);
 			}
+	}
+
+	void CorrectEffect() {
+		foreach(string value in animOn) {
+			anim2[0].SetBool (value, true);
+		}
+		foreach(string value in animOff) {
+			anim2[0].SetBool (value, false);
+		}
+		foreach(Transform value in activeOn) {
+			value.gameObject.SetActive (true);
+		}
+		foreach(Transform value in activeOff) {
+			value.gameObject.SetActive (false);
+		}
 	}
 }
---
+++ GameJam2018/Assets/Scripts/ObjectControl2.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectControl2 : MonoBehaviour {
+
+	private bool guiShow = false;
+	static bool pickedUp = false;
+	public AudioClip openDoorSound;
+	public AudioClip closeDoorSound;
+	public Transform onHand;
+
+	private GameObject objectPickedUp;
+	private Transform pickedUpObjectParent;
+	private GameObject objectLookedAt;
+
+	private RaycastHit hit;
+	private Vector3 fwd;
+	private float rayLength = 3.0f;
+
+	void Start() {
+		fwd = transform.TransformDirection(Vector3.forward);
+		guiShow = false;
+	}
+
+	void Update() {
+		fwd = transform.TransformDirection(Vector3.forward);
+		if(Physics.Raycast(transform.position, fwd, out hit, rayLength)) {
+			if(hit.collider.gameObject.tag == "OpenDoor") {
+				guiShow = true;
+				objectLookedAt = hit.collider.gameObject;
+				if(Input.GetKeyDown("e")) {
+					closeDoor(objectLookedAt);
+					guiShow = false;
+				}
+			}
+
+			else if(hit.collider.gameObject.tag == "ClosedDoor") {
+				guiShow = true;
+				objectLookedAt = hit.collider.gameObject;
+				if(Input.GetKeyDown("e")) {
+					openDoor(objectLookedAt);
+					guiShow = false;
+				}
+			}
+
+			else if(hit.collider.gameObject.tag == "LockedDoor") {
+				guiShow = true;
+			}
+
+			else if (hit.collider.gameObject.tag == "PickUpAble") {
+				guiShow = true;
+				objectPickedUp = hit.collider.gameObject;
+				if(Input.GetKeyDown("e") && (pickedUp == false)) {
+					pickUp(objectPickedUp);
+					pickedUp = true;
+					guiShow = false;
+				}
+
+				else if(Input.GetKeyDown("e") && (pickedUp == true)) {
+					drop(objectPickedUp);
+					pickedUp = false;
+					guiShow = false;
+				}
+			}
+
+			else if(Input.GetKeyDown("e") && (pickedUp == true)) {
+				drop(objectPickedUp);
+				pickedUp = false;
+				guiShow = false;
+			}
+
+			else {
+				guiShow = false;
+			}
+
+		}
+	}
+
+	void OnGUI() {
+		if(Physics.Raycast(transform.position, fwd, out hit, rayLength)) {
+			Rect R = new Rect ((float)(Screen.width / 2.0), (float)(Screen.height / 2.0), 100.0f, 25.0f);
+			if(hit.collider.gameObject.tag == "OpenDoor") {
+				if (guiShow == true) {
+					GUI.Box(R, "Close Door");
+				}
+			}
+
+
+			if(hit.collider.gameObject.tag == "ClosedDoor") {
+				if (guiShow == true) {
+					GUI.Box(R, "Open Door");
+				}
+			}
+
+			if(hit.collider.gameObject.tag == "LockedDoor") {
+				if (guiShow == true) {
+					GUI.Box(R, "Door Locked");
+				}
+			}
+
+			else if (hit.collider.gameObject.tag == "PickUpAble") {
+				if (guiShow == true && pickedUp == false) { 
+					GUI.Box(R, "Pick Up");
+				}
+			}
+		} 
+	}
+
+	void pickUp(GameObject oPU) {
+		GameObject object1;
+		object1 = oPU;
+		object1.GetComponent<Rigidbody>().useGravity = false;
+		object1.GetComponent<Rigidbody>().isKinematic = true;
+		object1.transform.position = onHand.position;
+		object1.transform.rotation = transform.rotation;
+		pickedUpObjectParent = object1.transform.parent;
+		object1.transform.parent = GameObject.Find("Player").transform;
+		object1.transform.parent= GameObject.Find("FirstPersonCharacter").transform;
+	}
+
+	void drop(GameObject object1) {
+		object1.transform.parent = pickedUpObjectParent;
+		object1.GetComponent<Rigidbody>().useGravity = true;
+		object1.GetComponent<Rigidbody>().isKinematic = false;
+	}
+
+	void openDoor(GameObject door) {
+		door.GetComponent<Animation>().Play("DoorOpen");
+		door.GetComponent<AudioSource>().PlayOneShot(openDoorSound);
+		objectLookedAt.tag = "OpenDoor";
+	}
+
+	void closeDoor(GameObject door) {
+		door.GetComponent<Animation>().Play("DoorClose");
+		door.GetComponent<AudioSource>().PlayOneShot(closeDoorSound);
+		objectLookedAt.tag = "ClosedDoor";
+	}
+
+}
+	
