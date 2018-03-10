73698582092cbb91c078ec39bf6ec2d3bc5f04c3
AlexanderWardlow <37199749+AlexanderWardlow@users.noreply.github.com>
2018-03-10 14:01:27 -0500

    Lvl3

--- GameJam2018/Assets/Scripts/PortalTeleportSameDir.cs
+++ GameJam2018/Assets/Scripts/PortalTeleportSameDir.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PortalTeleportSameDir : MonoBehaviour {
 	public Transform reciever;
 	public Transform player;
-	public Transform playerCam;
+	public Transform playerParent;
 
 	private bool playerInField = false;
 
@@ -13,16 +13,15 @@ public class PortalTeleportSameDir : MonoBehaviour {
 		if (playerInField) {
 			playerInField = false;
 			print ("ThisIsRunning");
-			Vector3 portalToPlayer = player.position - transform.position;
+			Vector3 portalToPlayer = playerParent.position - transform.position;
 			float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
 		
 			//player has moved into portal, teleport
 			if (dotProduct < 0f) {
 			    //Vector3 angleAdjust = player.eulerAngles + 0f * Vector3.up;
-				player.Rotate(Vector3.up, 180);
-				playerCam.Rotate (Vector3.up, 180);
+				playerParent.Rotate(Vector3.up, 180);
 				Vector3 positionOffset = portalToPlayer;
-				player.position = reciever.position + positionOffset;
+				playerParent.position = reciever.position + positionOffset;
 			}
 		}
 	}
---
+++ GameJam2018/Assets/Scripts/RotatePlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlayer : MonoBehaviour {
+
+	public float rotationSpeed = 10.0f;
+	public float cameraSpeed = 1000f;
+	public Transform playerCam;
+
+
+	void FixedUpdate () {
+		float h = rotationSpeed * Input.GetAxis ("Mouse X");
+		transform.Rotate(0, h, 0);
+		float v = cameraSpeed * Input.GetAxis ("Mouse Y");
+		v *= Time.deltaTime*2;
+		playerCam.Rotate(-v,0,0);
+	}
+}
