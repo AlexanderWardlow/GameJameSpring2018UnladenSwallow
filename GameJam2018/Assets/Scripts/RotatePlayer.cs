e1ba3aac2108c244ed520328c1c2e8ef01af1e31
AlexanderWardlow <37199749+AlexanderWardlow@users.noreply.github.com>
2018-03-10 15:55:23 -0500

    Lvl1Complete, except transition

--- GameJam2018/Assets/Scripts/RotatePlayer.cs
+++ GameJam2018/Assets/Scripts/RotatePlayer.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class RotatePlayer : MonoBehaviour {
 
-	public float rotationSpeed = 1000.0f;
-	public float cameraSpeed = 5000f;
+	public float rotationSpeed = 500.0f;
+	public float cameraSpeed = 10f;
 	public Transform playerCam;
 
 	private float verticalRotation;
 
 	void FixedUpdate () {
 		float h = rotationSpeed * Input.GetAxis ("Mouse X");
-		h *= Time.deltaTime * 10;
 		transform.Rotate(0, h, 0);
-		verticalRotation -= Input.GetAxis ("Mouse Y") * 5f * Time.deltaTime * 10;
+		verticalRotation -= Input.GetAxis ("Mouse Y") * 5f;
 		verticalRotation = Mathf.Clamp (verticalRotation, -90, 90);
 		playerCam.localRotation = Quaternion.Euler (verticalRotation, 0, 0);
 	}
