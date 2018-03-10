ecc4a95e2a4e4c3abaabbd9b090091e7b80243d3
falderol <27891045+falderol@users.noreply.github.com>
2018-03-10 14:09:12 -0500

    FixedButtonTermCode

--- GameJam2018/Assets/Scripts/ButtonTerminal.cs
+++ GameJam2018/Assets/Scripts/ButtonTerminal.cs
@@ -2,60 +2,149 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonTerminal : MonoBehaviour {
-	public Animator anim;
-	public string correctCubeColor;
-	public bool cantPickupAfter;
+public class ButtonTerminal : MonoBehaviour
+{
+    public Animator anim;
+    public string correctCubeColor;
+    public bool cantPickupAfter;
+    public bool undoCor;
+    public bool undoWrong;
 
-	public Animator[] anim2;
-	public string[] animOn;
-	public string[] animOff;
-	public Transform[] activeOn;
-	public Transform[] activeOff;
+    public Animator[] anim2;
+    public string[] CoranimOn;
+    public string[] CoranimOff;
+    public Transform[] CoractiveOn;
+    public Transform[] CoractiveOff;
+    public string[] WronganimOn;
+    public string[] WronganimOff;
+    public Transform[] WrongactiveOn;
+    public Transform[] WrongactiveOff;
 
-	void OnTriggerEnter (Collider other) {
-		//FIXME need to convert the objectControl to a c# class and check if object can be picked up
-		if (!anim.GetBool("boxPresent")) {
-			if (other.gameObject.tag == "PickUpAble") {
-				if (cantPickupAfter) {
-					other.gameObject.tag = "Untagged";
-				}
+    void OnTriggerEnter(Collider other)
+    {
+        print("wasup");
+        //FIXME need to convert the objectControl to a c# class and check if object can be picked up
+        if (!anim.GetBool("boxPresent"))
+        {
+            if (other.gameObject.tag == "PickUpAble")
+            {
+                if (cantPickupAfter)
+                {
+                    other.gameObject.tag = "Untagged";
+                }
 
-				anim.SetBool ("boxPresent", true);
-				if (other.gameObject.name.IndexOf(correctCubeColor) != -1) {
-					anim.SetBool ("CorrectAnswer", true);
-					anim.SetBool ("WrongAnswer", false);
-					CorrectEffect ();
+                anim.SetBool("boxPresent", true);
+                if (other.gameObject.name.IndexOf(correctCubeColor) != -1)
+                {
+                    
+                    anim.SetBool("CorrectAnswer", true);
+                    anim.SetBool("WrongAnswer", false);
+                    CorrectEffect();
 
-				}
-				else {
-					anim.SetBool ("CorrectAnswer", false);
-					anim.SetBool ("WrongAnswer", true);
-				}
-			}
-		}
-	}
+                }
+                else
+                {
+                    WrongEffect();
+                    anim.SetBool("CorrectAnswer", false);
+                    anim.SetBool("WrongAnswer", true);
+                }
+            }
+        }
+    }
 
-	void OnTriggerExit (Collider other) {
-			if (other.gameObject.tag == "PickUpAble") {
-				anim.SetBool ("CorrectAnswer", false);
-				anim.SetBool ("WrongAnswer", false);
-				anim.SetBool ("boxPresent", false);
-			}
-	}
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "PickUpAble")
+        {
+            if (anim.GetBool("CorrectAnswer") && undoCor) {
+                UndoCorrectEffect();
+            }
+            if (anim.GetBool("WrongAnswer") && undoWrong) {
+                UndoWrongEffect();
+            }
+            anim.SetBool("CorrectAnswer", false);
+            anim.SetBool("WrongAnswer", false);
+            anim.SetBool("boxPresent", false);
+        }
+    }
 
-	void CorrectEffect() {
-		foreach(string value in animOn) {
-			anim2[0].SetBool (value, true);
-		}
-		foreach(string value in animOff) {
-			anim2[0].SetBool (value, false);
-		}
-		foreach(Transform value in activeOn) {
-			value.gameObject.SetActive (true);
-		}
-		foreach(Transform value in activeOff) {
-			value.gameObject.SetActive (false);
-		}
-	}
+    void CorrectEffect()
+    {
+        foreach (string value in CoranimOn)
+        {
+            anim2[0].SetBool(value, true);
+        }
+        foreach (string value in CoranimOff)
+        {
+            anim2[0].SetBool(value, false);
+        }
+        foreach (Transform value in CoractiveOn)
+        {
+            value.gameObject.SetActive(true);
+        }
+        foreach (Transform value in CoractiveOff)
+        {
+            value.gameObject.SetActive(false);
+        }
+    }
+
+    void UndoCorrectEffect()
+    {
+        foreach (string value in CoranimOn)
+        {
+            anim2[0].SetBool(value, false);
+        }
+        foreach (string value in CoranimOff)
+        {
+            anim2[0].SetBool(value, true);
+        }
+        foreach (Transform value in CoractiveOn)
+        {
+            value.gameObject.SetActive(false);
+        }
+        foreach (Transform value in CoractiveOff)
+        {
+            value.gameObject.SetActive(true);
+        }
+    }
+
+    void WrongEffect()
+    {
+        foreach (string value in WronganimOn)
+        {
+            anim2[0].SetBool(value, true);
+        }
+        foreach (string value in WronganimOff)
+        {
+            anim2[0].SetBool(value, false);
+        }
+        foreach (Transform value in WrongactiveOn)
+        {
+            value.gameObject.SetActive(true);
+        }
+        foreach (Transform value in WrongactiveOff)
+        {
+            value.gameObject.SetActive(false);
+        }
+    }
+
+    void UndoWrongEffect()
+    {
+        foreach (string value in WronganimOn)
+        {
+            anim2[0].SetBool(value, false);
+        }
+        foreach (string value in WronganimOff)
+        {
+            anim2[0].SetBool(value, true);
+        }
+        foreach (Transform value in WrongactiveOn)
+        {
+            value.gameObject.SetActive(false);
+        }
+        foreach (Transform value in WrongactiveOff)
+        {
+            value.gameObject.SetActive(true);
+        }
+    }
 }
