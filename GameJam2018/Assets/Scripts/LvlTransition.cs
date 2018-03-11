5718dbb6d4cb3a5bcfe2c0aaa36217560c17d149
AlexanderWardlow <37199749+AlexanderWardlow@users.noreply.github.com>
2018-03-10 19:23:49 -0500

    AddingMoreTransitions

---
+++ GameJam2018/Assets/Scripts/LvlTransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LvlTransition: MonoBehaviour {
+	public Transform lv1;
+	public Transform lv2;
+	public Transform player;
+
+	void OnTriggerEnter(Collider other) {
+		lv1.gameObject.SetActive (false);
+	}
+
+	void OnTriggerExit(Collider other) {
+		lv2.gameObject.SetActive (true);
+		transform.gameObject.SetActive (false);
+	}
+
+}
